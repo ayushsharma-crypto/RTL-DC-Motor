06a0967c3482d4b1946aaa10c80ed334866e3385
Ayush Sharma <5460563-ayushsharma-crypto@users.noreply.gitlab.com>
2021-10-03 14:55:28 +0530

    some more backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 const PORT = 4000;
+const DB_NAME = "dcmotorDB"
+
+// routes
+var testAPIRouter = require("./routes/testAPI");
+var UserRouter = require("./routes/Users");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connection to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/' + DB_NAME, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully !");
+})
 
 // setup API endpoints
-app.get("/", function(req, res) {
-	res.send("API is working properly !");
-});
+app.use("/testAPI", testAPIRouter);
+app.use("/user", UserRouter);
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
