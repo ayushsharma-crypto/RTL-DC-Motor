5083d682e5bede9b13311a5851a2399185a8e85e
Ayush Sharma <5460563-ayushsharma-crypto@users.noreply.gitlab.com>
2021-11-01 15:50:02 +0530

    Authentication signup, signout, signin, check logged

--- Backend/server.js
+++ Backend/server.js
@@ -1,31 +1,36 @@
 const express = require('express');
 const app = express();
-// const bodyParser = require('body-parser');
-const cors = require('cors');
 const mongoose = require('mongoose');
+const passport = require("passport");
 const PORT = 4000;
 const DB_NAME = "dcmotorDB";
 const dotenv = require('dotenv');
+var session = require("express-session")
 
-// routes
-var experimentAPIRouter = require("./routes/experimentAPI");
-var UserRouter = require("./routes/Users");
 
-app.use(cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cors());
 app.use(express.json());  
 app.use(express.urlencoded({ extended: true }));
-
 dotenv.config();
+
+// setup for authentication
+app.use(session({
+    secret: `${process.env.SECRET}`,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // Connection to MongoDB
 try{
-    mongoose.connect(`${process.env.API_URL}`, { useNewUrlParser: true });
-const connection = mongoose.connection;
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully !");
-});
+    mongoose.connect(`${process.env.API_URL}`, { useNewUrlParser: true, useUnifiedTopology: true });
+    const connection = mongoose.connection;
+    connection.once('open', function() {
+        console.log("MongoDB database connection established successfully !");
+    });
 }
 catch (error){
     console.error(`Error: ${error.message}`)
@@ -33,11 +38,17 @@ catch (error){
 }
 
 
-// setup API endpoints
+// routes
+var experimentAPIRouter = require("./routes/experimentAPI");
+var UserRouter = require("./routes/Users");
+var BookingRouter = require("./routes/Booking");
 
+// setup API endpoints
 app.use("/experiment", experimentAPIRouter);
 app.use("/user", UserRouter);
+app.use("/booking",BookingRouter);
 
+// server check
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
