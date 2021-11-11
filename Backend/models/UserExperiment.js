cc75b695c9f2c930179245daddc953a930f7800e
umangS77 <umang.srivastava.777@gmail.com>
2021-11-11 16:23:38 +0530

    Changed user experiment schema

--- Backend/models/UserExperiment.js
+++ Backend/models/UserExperiment.js
@@ -1,39 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+ 
 const UserSchema = new Schema(
-    {
-        experimentData: [
-            {
-              request: {
-                voltage: {
-                  type: Number,
-                  required: true,
-                },
-              },
-              response: {
-                theoreticalRPM: {
-                  type: Number,
-                  default: null,
-                },
-                calculatedRPM: {
-                  type: Number,
-                  default: null,
-                },
-                powerConsumed: {
-                  type: Number,
-                  default: null,
-                },
-                latency: {
-                 type: Date,
-                  default: null,
-                },
-              },
+   {
+       experimentData: [
+         {
+            voltage: {
+              type: String,
+              required: true,
             }
-          ],
-          description : String,
-    }
+          },
+          {
+            calculatedRPM: {
+              type: String,
+              default: null,
+            }
+          },
+          {
+            averageCurrent : {
+              type : String,
+              default : null,
+            }
+          }
+         ],
+         description : String,
+   }
 );
-
+ 
 module.exports = Userexperiment = mongoose.model("UserExperiment", UserSchema);
-
